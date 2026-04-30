f061e1ce79c2dd6e84947883690b807dbc266c00
xuxin1955 <x2874836915@qq.com>
2026-04-30 16:30:02 +0800

    update

--- scripts/diy-part.sh
+++ scripts/diy-part.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd $GITHUB_WORKSPACE/openwrt || exit 1
+
 [ -f feeds/packages/net/onionshare-cli/Makefile ] || exit 0
 
 sed -i '/+python3-unidecode/d' feeds/packages/net/onionshare-cli/Makefile
