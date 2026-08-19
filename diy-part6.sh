88bc79152163a267eb1bf2cced4737e84c7ac1cc
xuxin1955 <x2874836915@qq.com>
2026-08-19 11:18:44 +0800

    update

--- diy-part6.sh
+++ diy-part6.sh
@@ -27,4 +27,4 @@ cp -r "$GITHUB_WORKSPACE/scripts/files-7628" "$GITHUB_WORKSPACE/openwrt/files"
 ls -R "$GITHUB_WORKSPACE/openwrt/files"
 
 # QModem
-echo "src-git qmodem https://github.com/FUjr/QModem.git;main" >> openwrt/feeds.conf.default
+echo "src-git qmodem https://github.com/FUjr/QModem.git;main" >> "$GITHUB_WORKSPACE/openwrt/feeds.conf.default"
