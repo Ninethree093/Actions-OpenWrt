0226f26ed5d39c6fd7b346b6087ceab52cd4946b
xuxin1955 <x2874836915@qq.com>
2026-08-22 22:39:13 +0800

    update

--- diy-part4.sh
+++ diy-part4.sh
@@ -4,8 +4,8 @@
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 # turboacc
-# curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
-curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+# curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 
 # temp
 git clone https://github.com/gSpotx2f/luci-app-temp-status package/luci-app-temp-status
