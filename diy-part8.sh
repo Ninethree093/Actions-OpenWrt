e84ff1ad4a16626823f1b2a879421afa98e594e5
xuxin1955 <x2874836915@qq.com>
2026-08-22 14:27:35 +0800

    update

--- diy-part8.sh
+++ diy-part8.sh
@@ -12,10 +12,3 @@ sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_gener
 # mkdir -p target/linux/ramips/dts/
 # cp -f "$GITHUB_WORKSPACE/scripts/mt7621/mt7621_hilink_hlk-7621a.dts" "target/linux/ramips/dts/mt7621_hilink_hlk-7621a.dts"
 
-
-# hlk7620A dts
-mkdir -p target/linux/ramips/dts/
-cp -f "$GITHUB_WORKSPACE/scripts/mt7620/mt7620a_glinet_gl-mt750.dts" "target/linux/ramips/dts/mt7620a_glinet_gl-mt750.dts"
-
-mkdir -p "target/linux/ramips/mt7620/base-files/etc/board.d/"
-cp -f "$GITHUB_WORKSPACE/scripts/mt7620/01_leds" "target/linux/ramips/mt7620/base-files/etc/board.d/01_leds"
