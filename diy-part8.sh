df5c08e919030af49eb30fe1d1c552eca25316b5
xuxin1955 <x2874836915@qq.com>
2026-08-22 09:32:27 +0800

    update

--- diy-part8.sh
+++ diy-part8.sh
@@ -5,9 +5,17 @@ sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_gener
 
 
 # hlk7621 dts
-mkdir -p target/linux/ramips/dts/
-cp -f "$GITHUB_WORKSPACE/scripts/mt7621/mt7621_hilink_hlk-7621a-evb.dts" "target/linux/ramips/dts/mt7621_hilink_hlk-7621a-evb.dts"
+# mkdir -p target/linux/ramips/dts/
+# cp -f "$GITHUB_WORKSPACE/scripts/mt7621/mt7621_hilink_hlk-7621a-evb.dts" "target/linux/ramips/dts/mt7621_hilink_hlk-7621a-evb.dts"
 
 # hlk7621 dts
+# mkdir -p target/linux/ramips/dts/
+# cp -f "$GITHUB_WORKSPACE/scripts/mt7621/mt7621_hilink_hlk-7621a.dts" "target/linux/ramips/dts/mt7621_hilink_hlk-7621a.dts"
+
+
+# hlk7620A dts
 mkdir -p target/linux/ramips/dts/
-cp -f "$GITHUB_WORKSPACE/scripts/mt7621/mt7621_hilink_hlk-7621a.dts" "target/linux/ramips/dts/mt7621_hilink_hlk-7621a.dts"
+cp -f "$GITHUB_WORKSPACE/scripts/mt7620/mt7620a_glinet_gl-mt750.dts" "target/linux/ramips/dts/mt7620a_glinet_gl-mt750.dts"
+
+mkdir -p "target/linux/ramips/mt7620/base-files/etc/board.d/"
+cp -f "$GITHUB_WORKSPACE/scripts/mt7620/01_leds" "target/linux/ramips/mt7620/base-files/etc/board.d/01_leds"
