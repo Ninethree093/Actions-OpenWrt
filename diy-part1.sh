e6b8951e1651f957e44eb2859ea116b03827f51e
xuxin1955 <x2874836915@qq.com>
2026-09-18 11:10:38 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,6 +3,7 @@
 # 8916dts
 mkdir -p target/linux/msm89xx/dts/
 cp -f "$GITHUB_WORKSPACE/scripts/dts/msm8916.dtsi" "target/linux/msm89xx/dts/msm8916.dtsi"
+cp -f "$GITHUB_WORKSPACE/scripts/dts/msm8916-ufi.dtsi" "target/linux/msm89xx/dts/msm8916-ufi.dtsi"
 
 # OpenAppFilter
 git clone --depth 1 https://github.com/destan19/luci-app-harbor-file.git package/harbor-file
@@ -10,11 +11,6 @@ git clone --depth 1 https://github.com/destan19/luci-app-harbor-file.git package
 # turboacc
 # curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 # curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
-
-
-# inspect
-echo ">>> OPP VOLTAGE COUNT: $(grep -c 'opp-microvolt' target/linux/msm89xx/dts/msm8916.dtsi) <<<"
-
 
 # kenzo
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
